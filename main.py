2e42da79c90bd4614ddb7f5efd456d093de64f0a
tivvit <tivvit@seznam.cz>
2014-05-10 17:18:27 +0200

    users api

--- main.py
+++ main.py
@@ -14,11 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         self.response.write('Hello world!')
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
